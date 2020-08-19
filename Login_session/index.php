bdc55128caccf3ae2d7364662b8623533efbe31c
owen5566 <owen5566@mail.com>
2020-08-19 17:47:34 +0800

    初版

---
+++ Login_session/index.php
@@ -0,0 +1,35 @@
+<?php
+    $userName="Guest";
+    $userPageHref = "login.php";
+    $loginHref = "login.php>登入";
+    session_start();
+    if(isset($_SESSION["userName"])){
+        $userName =  $_SESSION["userName"];
+        $userPageHref = "secret.php";
+        $loginHref = "login.php?logout=1>登出";
+    }
+    
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Lab - index</title>
+</head>
+<body>
+
+<table width="300" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#F2F2F2">
+  <tr>
+    <td align="center" bgcolor="#CCCCCC"><font color="#FFFFFF">會員系統 - 首頁</font></td>
+  </tr>
+  <tr>
+    <td align="center" valign="baseline"><a href=<?=$loginHref?></a> | <a href="<?=$userPageHref?>">會員專用頁</a></td>
+  </tr>
+  <tr>
+    <td align="center" bgcolor="#CCCCCC">Hello! <?=$userName?></td>
+  </tr>
+</table>
+
+
+</body>
+</html>
